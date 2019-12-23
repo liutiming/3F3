4144a685c82f903fb05ef39fd71ba4f78f9c0247
Ollie Barnard <obarnard99@gmail.com>
2019-12-23 10:27:31 +0000

    modified uniform dist

--- comparison.py
+++ comparison.py
@@ -2,61 +2,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 from pdfs import n_pdf, u_pdf
 from smoothing import ksdensity
+import math
 
 
-dist = 'u'
+dist = 'n'
 smoothed = True
-width = 0.4
+width = 0.1
+img_name = 'smoothed_normal01.png'
 
 
 def uniform_plot(a, b):
     p = u_pdf(a, b)
     range = b - a
-    plt.plot([a, b], [p, p], color='darkorange', ls='--')
+    plt.plot([a, b], [p, p], color='darkorange', ls='--', label='Exact Uniform Dist.')
     plt.plot([a - range / 4, a], [0, 0], color='darkorange', ls='--')
     plt.plot([b, b + range / 4], [0, 0], color='darkorange', ls='--')
     plt.plot([a, a], [0, p], color='darkorange', ls='--')
     plt.plot([b, b], [0, p], color='darkorange', ls='--')
 
 
-def normal_plot(x_axis):
-    plt.plot(x_axis, n_pdf(x_axis), color='darkorange', ls='--')
-
-
 if dist == 'n':
     # Plot normal distribution
-    bins = 30
+    J = 30
     x_range = 10
     N = 1000
     x_rand = np.random.randn(N)
     x_lin = np.linspace(-(x_range / 2), x_range / 2, N)
     if smoothed:
         ks_density = ksdensity(x_rand, width=width)
-        plt.plot(x_lin, ks_density(x_lin)/width, color='cornflowerblue')
-        img_name = 'smoothed_normal.png'
+        plt.plot(x_lin, ks_density(x_lin), color='cornflowerblue', label='Smoothed Approximation')
     else:
-        plt.hist(x_rand, bins=bins, color='cornflowerblue', density=True)
-        img_name = 'normal_comparison.png'
-    normal_plot(x_lin)
+        plt.hist(x_rand, bins=J, color='cornflowerblue', density=True, label='Histogram Approximation')
+    plt.plot(x_lin, n_pdf(x_lin), color='darkorange', ls='--', label='Exact Normal Dist.') # normal plot
+    plt.legend()
 
 elif dist == 'u':
     # Plot uniform distribution
     a = 0
     b = 1
     p = u_pdf(a, b)
-    bins = 20
+    J = 20
     N = 1000
     x_rand = np.random.rand(N)
     x_lin = np.linspace(-0.5, 1.5, N)
     if smoothed:
         ks_density = ksdensity(x_rand, width=width)
-        plt.plot(x_lin, ks_density(x_lin)/width, color='cornflowerblue')
-        img_name = 'smoothed_uniform.png'
+        plt.plot(x_lin, ks_density(x_lin), color='cornflowerblue', label='Smoothed Approximation')
     else:
-        plt.hist(x_rand, bins=bins, color='cornflowerblue', density=True)
-        img_name = 'uniform_comparison.png'
+        plt.hist(x_rand, bins=J, color='cornflowerblue', density=True, label='Histogram Approximation')
     uniform_plot(a, b)
-    # normal_plot(x_lin)
+    #plt.plot(x_lin, n_pdf(x_lin), color='red', ls='--') # normal plot
+    plt.legend()
 
 plt.xlabel('x')
 plt.ylabel('Normalised Count')
