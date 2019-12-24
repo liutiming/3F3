13881088ea4473321412d3c40d1201bcd7612f44
Ollie Barnard <obarnard99@gmail.com>
2019-12-24 17:47:45 +0000

    Implement second transform

--- transform_plots.py
+++ transform_plots.py
@@ -3,21 +3,28 @@ import matplotlib.pyplot as plt
 from pdfs import n_pdf
 from transforms import *
 
-transform = 1
-J = 20
+transform = 2
+J = 30
 N = 1000
 img_name = 'transform{}.png'.format(transform)
 
+x_rand = np.random.randn(N)
 if transform == 1:
     # y = ax + b
     a = 2
     b = 4
     f = t1(a, b)
-    x_rand = np.random.randn(N)
     x_lin = np.linspace(-(10 * a) / 2 + b, (10 * a) / 2 + b, N)
-    plt.hist(f(x_rand), bins=J, color='cornflowerblue', density=True, label='Histogram Approximation')
-    plt.plot(x_lin, n_pdf(x_lin, b, a), color='darkorange', ls='--', label='Exact Transformed Dist.') # normal plot
+    y = n_pdf(x_lin, b, a)
 
+elif transform == 2:
+    # y = x^2
+    f = t2()
+    x_lin = np.linspace(-4, 3, N)
+    y = t2_plot(x_lin)
+
+plt.plot(x_lin, y, color='darkorange', ls='--', label='Exact Transformed Dist.')
+plt.hist(f(x_rand), bins=J, color='cornflowerblue', density=True, label='Histogram Approximation')
 plt.legend()
 plt.xlabel('x')
 plt.ylabel('Normalised Count')
--- transforms.py
+++ transforms.py
@@ -1,5 +1,29 @@
+import numpy as np
+
+
 def t1(a, b):
     """y = ax + b"""
+
     def f(x):
-        return a*x + b
+        return a * x + b
+
     return f
+
+
+def t2():
+    """y = x^2"""
+
+    def f(x):
+        return x * x
+
+    return f
+
+
+def t2_plot(x):
+    y = np.zeros(len(x))
+    for idx, xi in enumerate(x):
+        if xi > 0.05: # Prevent very large values close to 0
+            y[idx] = (1 / (np.sqrt(2 * np.pi * xi))) * np.exp(-xi / 2)
+        else:
+            y[idx] = 0
+    return y
